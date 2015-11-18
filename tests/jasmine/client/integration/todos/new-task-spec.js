c3fdb7515eb6ba29da339b196de1ed45fce925c6
Nick Riebeek <nick.riebeek@gmail.com>
2015-11-17 19:25:05 -0500

    fixed new task test that got mangled

--- tests/jasmine/client/integration/todos/new-task-spec.js
+++ tests/jasmine/client/integration/todos/new-task-spec.js
@@ -9,18 +9,18 @@ describe ("the todo page : new task field", function() {
     Meteor.setTimeout(function() {
       addTaskViaUI('My new task');
       
+      // check the UI
+      var tasks = TodosSpecHelper.retrieveTasksFromUI(); // use the helper
+      expect(tasks.length).toEqual(1);
+      expect(tasks[0]).toEqual('My new task');
+      
       // check the DB
       var task = Tasks.findOne({text: 'My new task'});
       expect(task).not.toBe(undefined);
       expect(task.text).toEqual('My new task');
       expect(task.completed).toBe(false);
       done();
-
-      // check the UI
-      var tasks = TodosSpecHelper.retrieveTasksFromUI(); // use the helper
-      expect(tasks.length).toEqual(1);
-      expect(tasks[0]).toEqual('My new task');
-    }, 600);
+    }, 400);
   });
 
   it ("should clear out the new task field on form submit", function(done) {
@@ -28,7 +28,7 @@ describe ("the todo page : new task field", function() {
       addTaskViaUI('Another new task');
       expect($('.new-task input').val()).toEqual('');
       done();
-    }, 600);
+    }, 400);
   });
   
 });
